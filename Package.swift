39a97aac3b99b63fea07bff8767fc3310f292dcc
Firdavs Khaydarov <recouse@recouse.email>
2025-01-25 03:14:29 +0200

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "ValuePicker",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(name: "ValuePicker", targets: ["ValuePicker"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "ValuePicker",
+            dependencies: [],
+            path: "Sources")
+    ]
+)
